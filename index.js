2d9cfc24678ae9cbfde4af05d5bace7d7aea812f
Bhavykun <67602618+Bhavykun@users.noreply.github.com>
2024-04-18 15:28:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user : "postgres",
   host : "localhost",
   database : "permalist",
-  password : "kiritoXasuna",
+  password : "password",
   port : 5432
 });
 db.connect();
